9a9052dde3f236d936d07cdb09196a6562a443bf
Doan Khue <doankhue09@gmail.com>
2019-05-20 17:54:44 +0700

    operator

--- number.py
+++ number.py
@@ -4,11 +4,21 @@ expression1 = a + b
 expression2 = 50 - 5*6
 expression3 = (50 - 5*6)/4
 expression4 = 8/5
+# // expression
+expression5 = 17//3
+expression6 = 17%3
+# calculate powers
+expression7 = 2 ** 3
 print(expression1)
 print(expression2)
 print(expression3)
 print(expression4)
+print(expression5)
+print(expression6)
+print(expression7)
 print(type(expression4))
 print(type(expression2))
+
+
 
 
