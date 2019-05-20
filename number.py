216a69f2bc1286991e5f85fa4aed3abc7d412503
Doan Khue <doankhue09@gmail.com>
2019-05-20 14:32:29 +0700

    number

---
+++ number.py
@@ -0,0 +1,14 @@
+a = 2
+b = 2
+expression1 = a + b
+expression2 = 50 - 5*6
+expression3 = (50 - 5*6)/4
+expression4 = 8/5
+print(expression1)
+print(expression2)
+print(expression3)
+print(expression4)
+print(type(expression4))
+print(type(expression2))
+
+
