8d76d82051efcbcda9075d06acddfd247165dfc3
Doan Khue <doankhue09@gmail.com>
2019-05-21 22:24:21 +0700

    print escape "

--- string.py
+++ string.py
@@ -9,3 +9,14 @@ print(string2)
 string3 = 'doesn\'t'
 
 print(string3)
+
+string4 = '"Yes," they said'
+
+print(string4)
+
+string5 = '"Isn\'t" they said '
+
+print(string5)
+
+print('C:\some\name')
+print(r'C:\some\name')
